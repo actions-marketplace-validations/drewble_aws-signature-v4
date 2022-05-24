bc8b32b4330c01cf9a5c78c47dd3eaa9bcd0559b
Drew Blake <drew.blake@lonelyplanet.com>
2022-05-24 17:21:33 -0500

    Fix bug in filterObject function

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function filterObject(obj) {
   const filtered = {};
   for(const el in obj) {
     if (el !== undefined) {
-      filtered[el] = el;
+      filtered[el] = obj[el];
     }
   }
   return filtered;
